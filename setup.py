8621d50e284aaec08410e304d5f9c6255304b379
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2023-10-22 23:16:51 -0400

    🦌🦌🐐🐐🐃🐕🐕

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import Extension, setup
 
 if __name__ == '__main__':
-    setup(name="namespaces.py", license="MIT", ext_modules=[Extension("namespaces", ["./mod.c"])],)
+    setup(name="namespaces.py", version="1.0.1", license="MIT", ext_modules=[Extension("namespaces", ["./mod.c"])],)
